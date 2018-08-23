1f49fe2fbb9d746d703eb185bdca4f7e21c0dd91
alincode <alincode@gmail.com>
2018-08-24 00:14:50 +0800

    update fitbit

--- fitbit.md
+++ fitbit.md
@@ -2,6 +2,11 @@
 
 * [Fitbit Development: Using OAuth 2.0](https://dev.fitbit.com/build/reference/web-api/oauth2/)
 * [ngrok - secure introspectable tunnels to localhost](https://ngrok.com/)
+
+```
+ngrok http 9966
+```
+
 * [URL Encoder &amp; Decoder - Free Online Text Conversion](http://www.togglecase.com/url_encode_decode)
 
 ### example
--- src/fitbit.js
+++ src/fitbit.js
@@ -3,7 +3,7 @@ var CLIENT_ID = '112233';
 var NGROK_SUB_DOMAIN = '123456';
 
 if (!window.location.hash) {
-  window.location.replace(`https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=${CLIENT_ID}&redirect_uri=https%3A%2F%2F${NGROK_SUB_DOMAIN}.ngrok.io%2F&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight`);
+  window.location.replace(`https://www.fitbit.com/oauth2/authorize?response_type=token&expires_in=31536000&client_id=${CLIENT_ID}&redirect_uri=https%3A%2F%2F${NGROK_SUB_DOMAIN}.ngrok.io%2F&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight`);
 } else {
   var fragmentQueryParameters = {};
   window.location.hash.slice(1).replace(
