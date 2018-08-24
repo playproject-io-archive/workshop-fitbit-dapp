717b083036e0da923b463de80e610c0340b269e8
alincode <alincode@gmail.com>
2018-08-24 15:08:33 +0800

    update fitbit

--- fitbit.md
+++ fitbit.md
@@ -1,6 +1,16 @@
 # fitbit
 
 * [Fitbit Development: Using OAuth 2.0](https://dev.fitbit.com/build/reference/web-api/oauth2/)
+* [create Fibit app account](https://dev.fitbit.com/apps/new)
+
+### example
+
+* [jeremiahlee/fitbit-web-demo](https://github.com/jeremiahlee/fitbit-web-demo/blob/master/boot.js)
+* [thegameofcode/passport-fitbit-oauth2](https://github.com/thegameofcode/passport-fitbit-oauth2)
+
+
+### Mise
+
 * [ngrok - secure introspectable tunnels to localhost](https://ngrok.com/)
 
 ```
@@ -8,8 +18,3 @@ ngrok http 9966
 ```
 
 * [URL Encoder &amp; Decoder - Free Online Text Conversion](http://www.togglecase.com/url_encode_decode)
-
-### example
-
-* [jeremiahlee/fitbit-web-demo](https://github.com/jeremiahlee/fitbit-web-demo/blob/master/boot.js)
-* [thegameofcode/passport-fitbit-oauth2](https://github.com/thegameofcode/passport-fitbit-oauth2)
--- src/fitbit.js
+++ src/fitbit.js
@@ -1,9 +1,9 @@
 var fitbitAccessToken;
-var CLIENT_ID = '112233';
-var NGROK_SUB_DOMAIN = '123456';
+var CLIENT_ID = '22CYSG';
+var EXPIRES_IN = 31536000;
 
 if (!window.location.hash) {
-  window.location.replace(`https://www.fitbit.com/oauth2/authorize?response_type=token&expires_in=31536000&client_id=${CLIENT_ID}&redirect_uri=https%3A%2F%2F${NGROK_SUB_DOMAIN}.ngrok.io%2F&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight`);
+  window.location.replace(`https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=https%3A%2F%2Falincode.github.io%2Fdevon4&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=${EXPIRES_IN}`);
 } else {
   var fragmentQueryParameters = {};
   window.location.hash.slice(1).replace(
@@ -14,3 +14,36 @@ if (!window.location.hash) {
   fitbitAccessToken = fragmentQueryParameters.access_token;
   document.body.innerHTML = fragmentQueryParameters;
 }
+
+var processResponse = function (res) {
+  if (!res.ok) {
+    throw new Error('Fitbit API request failed: ' + res);
+  }
+
+  var contentType = res.headers.get('content-type')
+  if (contentType && contentType.indexOf("application/json") !== -1) {
+    return res.json();
+  } else {
+    throw new Error('JSON expected but received ' + contentType);
+  }
+}
+
+var getProfile = function(profile) {
+  console.log(profile.user.age);
+  return profile;
+}
+
+fetch(
+  'https://api.fitbit.com/1/user/-/profile.json',
+  {
+    headers: new Headers({
+      'Authorization': 'Bearer ' + fitbitAccessToken
+    }),
+    mode: 'cors',
+    method: 'GET'
+  }
+).then(processResponse)
+  .then(getProfile)
+  .catch(function (error) {
+    console.error(error);
+  });
