bcfb8b10121b64bfb2d8b1edf59356c6dd6e5d38
alincode <alincode@gmail.com>
2018-09-16 22:19:35 +0800

    add more error tip

--- src/index.js
+++ src/index.js
@@ -86,6 +86,39 @@ const css = csjs`
     padding: 5px;
     width: 150px;
   }
+
+  .info, .success, .warning, .error, .validation {
+			border: 1px solid;
+			margin: 10px 0px;
+			padding: 15px 10px 15px 50px;
+			background-repeat: no-repeat;
+			background-position: 10px center;
+		}
+		.info {
+			color: #00529B;
+			background-color: #BDE5F8;
+			background-image: url('https://i.imgur.com/ilgqWuX.png');
+		}
+		.success {
+			color: #4F8A10;
+			background-color: #DFF2BF;
+			background-image: url('https://i.imgur.com/Q9BGTuy.png');
+		}
+		.warning {
+			color: #9F6000;
+			background-color: #FEEFB3;
+			background-image: url('https://i.imgur.com/Z8q7ww7.png');
+		}
+		.error{
+			color: #D8000C;
+			background-color: #FFBABA;
+			background-image: url('https://i.imgur.com/GnyDvKN.png');
+		}
+		.validation {
+			color: #D63301;
+			background-color: #FFCCBA;
+			background-image: url('https://i.imgur.com/GnyDvKN.png');
+		}
 `
 
 /******************************************************************************
@@ -126,7 +159,7 @@ const fundAreaElement = bel`
 function errorRender(errorMessage) {
   console.error(errorMessage);
   document.body.appendChild(bel`
-  <div class=${css.box} id="app">
+  <div class=${css.error} id="app">
     ${errorMessage}
   </div>
  `)
@@ -307,6 +340,7 @@ function getMyAddress(result) {
   web3.eth.defaultAccount = web3.eth.accounts[0];
   log('loading (1/7) - getMyAddress')
   web3.eth.getAccounts((err, localAddresses) => {
+    if (!localAddresses) return errorRender('You must be have MetaMask or local RPC endpoint.');
     localStorage.wallet = localAddresses[0];
     if (err) return done(err)
     result.wallet = localAddresses[0];
