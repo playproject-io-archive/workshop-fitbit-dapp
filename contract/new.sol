a96882c54174e9b3bb2ec7c5cc0c34e4fffda27a
alincode <alincode@gmail.com>
2018-10-02 22:53:01 +0800

    update contract

--- contract/new.sol
+++ contract/new.sol
@@ -30,10 +30,9 @@ contract CommonMixin {
 
 contract FunderMixin is CommonMixin {
     
-    uint private numFunders;
     uint private fundersOfAmount;
     
-    address[] funderAddresses;
+    address[] funderIndexs;
     mapping (address => Funder) funders;
 
     modifier minimizeContribute { require( msg.value >= 0.5 ether, "ether not enough"); _; }
@@ -46,7 +45,7 @@ contract FunderMixin is CommonMixin {
     }
     
     function getAllFunders() external view returns (address[]) {
-        return funderAddresses;
+        return funderIndexs;
     }
     
     function getFunder(address addr) public view returns (string, uint) {
@@ -54,11 +53,11 @@ contract FunderMixin is CommonMixin {
     }
     
     function getFunders() public view returns (string[], uint[]) {
-        string[] memory names = new string[](funderAddresses.length);
-        uint[] memory amounts = new uint[](funderAddresses.length);
+        string[] memory names = new string[](funderIndexs.length);
+        uint[] memory amounts = new uint[](funderIndexs.length);
         
-        for (uint i = 0; i < funderAddresses.length; i++) {
-            Funder storage funder = funders[funderAddresses[i]];
+        for (uint i = 0; i < funderIndexs.length; i++) {
+            Funder storage funder = funders[funderIndexs[i]];
             names[i] = funder.name;
             amounts[i] = funder.amount;
         }
@@ -68,7 +67,7 @@ contract FunderMixin is CommonMixin {
     
     // 贊助人數
     function getNumFunders() public view returns (uint) {
-        return numFunders;
+        return funderIndexs.length;
     }
     
     // 贊助總金額
@@ -83,8 +82,7 @@ contract FunderMixin is CommonMixin {
             funders[msg.sender].name = _name;
         } else {
             funders[msg.sender] = Funder(msg.sender, msg.value, now, _name);
-            numFunders++;
-            funderAddresses.push(msg.sender);
+            funderIndexs.push(msg.sender);
         }
         fundersOfAmount += msg.value;
     }
@@ -93,17 +91,14 @@ contract FunderMixin is CommonMixin {
 contract PlayerMixin is usingOraclize, CommonMixin {
     
     uint private constant GAS_LIMIT = 800000;
-    uint private minimizeSinupAmount = 0.1 ether;
-    uint private numPlayers;
     uint private playersOfAmount;
-    
-    mapping (uint => address) playerIndexs;
+    address[] public playerIndexs;
     mapping (address => Player) players;
     mapping (bytes32 => SignData) private signDatas;
     
     modifier onlyOraclize { require(msg.sender == oraclize_cbAddress(), "only oraclize"); _; }
     modifier onlySigned { require( isSigned(msg.sender), "only signed"); _; }
-    modifier minimizeSignup { require( msg.value >= minimizeSinupAmount, "ether not enough"); _; }
+    modifier minimizeBet { require( msg.value >= 0.1 ether, "ether not enough"); _; }
     modifier isNewPlayer { require(!isSigned(msg.sender), "you already signed"); _; }
     
     // ===>>> event
@@ -142,7 +137,7 @@ contract PlayerMixin is usingOraclize, CommonMixin {
     
     function getPlayersOfAmount() public view returns (uint) { return playersOfAmount; }
     
-    function getNumPlayers() public view returns (uint) { return numPlayers;  }
+    function getNumPlayers() public view returns (uint) { return playerIndexs.length;  }
     
     function getBeginStep(address addr) public view returns (uint) { return players[addr].beginStep; }
     
@@ -153,31 +148,20 @@ contract PlayerMixin is usingOraclize, CommonMixin {
     }
     
     // Step1
-    function requestActivities(string _encryptHeader, string _userId) public payable {
-        request("json(QmdKK319Veha83h6AYgQqhx9YRsJ9MJE7y33oCXyZ4MqHE).lifetime.total.steps",
-                "GET",
-                "https://api.fitbit.com/1/user/-/activities.json",
-                _encryptHeader,
-                _userId);
-    }
-    
-    // Step2
-    function request(string _query, string _method, string _url, string _encryptHeader, string _userId) public payable {
+    function request(string _encryptHeader, string _userId) public payable {
+        string memory _query = "json(QmdKK319Veha83h6AYgQqhx9YRsJ9MJE7y33oCXyZ4MqHE).lifetime.total.steps";
+        string memory _method = "GET";
+        string memory _url = "https://api.fitbit.com/1/user/-/activities.json";
         bytes32 queryId = oraclize_query("computation", [ _query, _method, _url, _encryptHeader ], GAS_LIMIT);
         signDatas[queryId] = SignData(_userId, msg.sender, msg.value, _encryptHeader);
     }
     
-    // Step3
+    // Step2
     function __callback(bytes32 _queryId, string _result, bytes _proof) public onlyOraclize {
         SignData memory o = signDatas[_queryId];
         emit LOG_OraclizeCallbackStep(o.userId, _queryId, parseInt(_result), _proof);
-        callback_ForGetUserStep(o.userId, _queryId, parseInt(_result), _proof);
-    }
-    
-    // Step4
-    function callback_ForGetUserStep(string _userId, bytes32 _queryId, uint steps, bytes _proof) private {
-        SignData memory o = signDatas[_queryId];
-        if(isSigned(o.addr)) {
+        uint steps = parseInt(_result);
+         if(isSigned(o.addr)) {
             players[o.addr].endStep = steps;
         } else {
             addPlayer(o.addr, o.amount, o.userId, o.encryptHeader, steps);
@@ -190,22 +174,22 @@ contract PlayerMixin is usingOraclize, CommonMixin {
     }
 
     // 註冊
-    function signup(string _encryptHeader, string _userId) public minimizeSignup onlyOnTime isNewPlayer payable {
-        requestActivities(_encryptHeader, _userId);
+    function signup(string _encryptHeader, string _userId) public minimizeBet onlyOnTime isNewPlayer payable {
+        request(_encryptHeader, _userId);
     }
 
     function updateAllUserStep() public onlyOwner payable {
         for(uint i = 0; i < getNumPlayers(); i++) {
             Player memory player = players[playerIndexs[i]];
-            requestActivities(player.encryptHeader, player.userId);
+            request(player.encryptHeader, player.userId);
         }
     }
     
     function addPlayer(address _addr, uint _amount, string _userId, string _encryptHeader, uint _beginStep) private {
         players[_addr] = Player(_addr, _amount, _userId, now, _beginStep, 0, false, _encryptHeader);
-        playerIndexs[numPlayers] = _addr;
+        playerIndexs.push(_addr);
         playersOfAmount += _amount;
-        numPlayers++;
+        
     }
 }
 
@@ -213,13 +197,13 @@ contract FitnessContest is PlayerMixin, FunderMixin {
     
     uint private doneAt;
     uint private constant GOAL_STEP = 10000 * duration;
-    address[] private winners;
+    address[] private winnerIndexs;
     Status private status;
     enum Status { Started, Ended, Withdrawal}
     
     modifier availableRefund () {  require(isAvailableRefund(), "you didn't signup."); _; }
     modifier availableAward {
-        require(isEnded(), "the contest is not end yet.");
+        require(status == Status.Ended, "the contest is not end yet.");
         require(isSigned(msg.sender) || (msg.sender == owner));
         require(now > doneAt + 10 minutes);
          _;
@@ -229,8 +213,8 @@ contract FitnessContest is PlayerMixin, FunderMixin {
         status = Status.Started;
     }
     
-    function isEnded() public view returns (bool) {
-        return status == Status.Ended;
+    function getStatus() public view returns (uint) {
+        return uint(status);
     }
 
     function getTotalAmount() public view returns (uint) {
@@ -246,20 +230,20 @@ contract FitnessContest is PlayerMixin, FunderMixin {
     function calculatorWinners() private {
         for(uint i = 0; i < getNumPlayers(); i++) {
             if (isWinner(playerIndexs[i])) {
-                winners.push(playerIndexs[i]);
+                winnerIndexs.push(playerIndexs[i]);
             }
         }
     }
     
     function getWinnerWithdrawalAmount() public view returns (uint) {
-        return this.balance.div(winners.length);
+        return this.balance.div(winnerIndexs.length);
     }
     
     function playersWithdrawal() private {
-        if(winners.length > 0){
+        if(winnerIndexs.length > 0){
             uint averageAmount = getWinnerWithdrawalAmount();
-            for(uint i = 0; i < winners.length; i++) {
-                winners[i].transfer(averageAmount);
+            for(uint i = 0; i < winnerIndexs.length; i++) {
+                winnerIndexs[i].transfer(averageAmount);
             }    
         }
     }
@@ -274,9 +258,9 @@ contract FitnessContest is PlayerMixin, FunderMixin {
     
     // owner Step2
     function award() public availableAward payable {
-        status = Status.Withdrawal;
         calculatorWinners();
         playersWithdrawal();
+        status = Status.Withdrawal;
     }
     
     function playerRefund() public availableRefund {
